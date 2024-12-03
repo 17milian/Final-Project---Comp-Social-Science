c09aa7010e641e5706ca4ac08c29b1d39d031ae8
Carlos Rodriguez Milian <117548093+17milian@users.noreply.github.com>
2024-12-03 13:08:24 -0500

    Add files via upload

---
+++ Food-Insecurity-Map/original_app.R
@@ -0,0 +1,50 @@
+#Note to Self: ALWAYS pull changes from github before modifying and push AFTER modifying
+#WebApp link: https://ray00126.shinyapps.io/food-insecurity-map/
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+library(usmap)
+library(ggplot2)
+library(dplyr)
+library(tidycensus)
+library(rsconnect)
+
+state_data <- read.csv("states_FI_graph.csv")
+county_data <- read.csv("counties_FI_graph.csv")
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      #options menu
+      selectInput(inputId = "region_type",choices = c("states","counties"), 
+      label = "Choose a region type"),
+      selectInput(inputId = "year", choices = c(as.character(2009:2022)), label = "Select a year")
+    ),
+    mainPanel(
+      "Map", plotOutput("usplot", height = 600)
+    )
+  )
+)
+#as.character(2009:2022)
+# builds the map
+getUsMap <- function(level, year) {
+  d <- state_data
+  title <- "US States"
+  if(level == "counties"){
+    d <- county_data
+    title <- "US Counties"
+  }
+  plot_usmap(data = d, values = paste("X",year, sep = ""), regions = level) + 
+    labs(title = title,
+         subtitle = "This is a blank map of the counties of the United States.") + 
+    theme(panel.background = element_rect(color = "black", fill = "lightblue")) 
+}
+server <- function(input, output){
+  output$usplot = renderPlot(getUsMap(input$region_type,input$year))
+}
+shinyApp(ui = ui,server = server)
